0682b5e56eb2c0876427a9fae31ef599504603bc
cornelcristes <petrucornel.cristea@gmail.com>
2022-11-24 23:07:25 +0200

    update ex5 with cinemagia option

--- ex5/5_movies_with_high_rating.py
+++ ex5/5_movies_with_high_rating.py
@@ -5,70 +5,88 @@ from requests import get as rg
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 
+
 # TO DO:
-    # take movie year
-    # take movie genre
-    # take take movie duration
-    # take movie url
-    # create custom function for cinemagia
+# take movie year
+# take movie genre
+# take movie duration
+# take movie url
 
-# take only movies with rating higher than rating from user
-def get_movies_by_rating(website, rating, file):
-    website = int(website)
+# take movies title from html code
+def get_movies_title(doc, website):
+    movies_title = []
+    # website : 1 - imbd, 2 - cinemagia
     if website == 1:
-        movie_dict = get_movies_imdb(rating)
-    elif website == 2:
-        # movie_dict = get_movies_cinemagia(rating)
-        msg = ('Option not available yet.')
-        return ValueError(msg)
+        html_class = 'lister-item-header'
+        title_list = doc.find_all('h3',{'class': html_class})
     else:
-        msg = ('ERROR: Website invalid.')
-        return ValueError(msg)
+        html_class = 'title'
+        title_list = doc.find_all('div',{'class': html_class})
 
-    get_csv_file(movie_dict, file)
-    
-# take movie titles from html code
-def get_movie_titles_imdb(doc, titles):
-    title_html_class = 'lister-item-header'
-    title_list = doc.find_all('h3',{'class': title_html_class})
-    for tag in title_list:
-        title_tag = tag.find('a').text
-        titles.append(title_tag)
-    return titles
+    for title in title_list:
+        if hasattr(title.find('a'),'text'):
+            title_val = title.find('a').text
+            movies_title.append(title_val)
 
-# take movie ratings from html code
-def get_movie_ratings_imdb(doc, rating):
-    rating_html_class = 'inline-block ratings-imdb-rating'
-    rating_list = doc.find_all('div',{'class': rating_html_class})
-    for tag in rating_list:
-        rating_tag = float(tag.get_text().strip())
-        rating.append(rating_tag)
-    return rating
+    return movies_title
+
+# take movies rating from html code
+def get_movies_rating(doc, website):
+    movies_rating = []
+    # website : 1 - imbd, 2 - cinemagia
+    if website == 1:
+        html_class = 'inline-block ratings-imdb-rating'
+        rating_html = doc.find_all('div',{'class': html_class})
+    else:
+        html_class = 'rating-cinemagia'
+        rating_html = doc.find_all('div',{'class': html_class})
+
+    for rating in rating_html:
+        try:
+            rating_val = float(rating.get_text().strip())
+        except:
+            rating_val = float(0)
+        movies_rating.append(rating_val)
+
+    return movies_rating
 
 # create movies dictionary
-def get_movies_imdb(rating):
-    rating = float(rating)
-    movies_dict = {'title':[], 'rating':[]}
-    titles = []
-    ratings = []
-    print('Read movies from webpage...\nPlease wait.')
-    for page in range(1,2000,100):
-        try:
-            url = 'https://www.imdb.com/search/title/?groups=top_1000&sort=user_rating,desc&count=100&start=' + str(page) + '&ref_=adv_next'
-            doc = get_html_code(url)
-            titles = get_movie_titles_imdb(doc, titles)
-            ratings = get_movie_ratings_imdb(doc, ratings)
-        except:
-            break
+def get_movies_list(website, rating):
+    movies_dict = {'Movie':[], 'Rating':[]}
+    titles_list  = []
+    ratings_list = []
+    print('Reading movies from webpage...\nPlease wait.')
+    # website : 1 - imbd, 2 - cinemagia
+    if website == 1:
+        website_url = 'https://www.imdb.com/search/title/?groups=top_1000&start='
+        pages = range(1,101,50)
+    else:
+        website_url = 'https://www.cinemagia.ro/filme/?pn='
+        pages = range(1,100)
+        
+    for page_number in pages:  
+        page_url = website_url + str(page_number)
+        doc = get_html_code(page_url)
+        # create list with all titles
+        titles = get_movies_title(doc, website)
+        titles_list = titles_list + titles
+        # create list with all ratings
+        ratings = get_movies_rating(doc, website)
+        ratings_list = ratings_list + ratings
 
-    for i in range(len(ratings)):
-        movie_rating = float(ratings[i])
+    for i in range(len(ratings_list)):
         # check if movie rating is higher than rating selected by user
-        if movie_rating > rating: 
-            movies_dict['title'].append(titles[i])
-            movies_dict['rating'].append(ratings[i])
+        if ratings_list[i] > rating: 
+            movies_dict['Movie'].append(titles_list[i])
+            movies_dict['Rating'].append(ratings_list[i])
 
     return movies_dict
+
+
+# take only movies with rating higher than rating from user
+def get_movies(website, rating, file):    
+    movies_dict = get_movies_list(website, rating)
+    get_file(movies_dict, file, website)
 
 # get parsed html source code from webpage
 def get_html_code(url):
@@ -79,26 +97,30 @@ def get_html_code(url):
     doc = bs(response.text, 'html.parser')
     return doc
 
-# create csv file with results
-def get_csv_file(movies_dict, file):
+# create file with results
+def get_file(movies_dict, file, website):
     data = pd.DataFrame(movies_dict)
-    data.head()
-    file = int(file)
+
+    if website == 1:
+        tag_name = '_imbd'
+    else:
+        tag_name = '_cinemagia'
+
     try:
         if file == 1:
-            data.to_csv('movies.csv',index=None)
-            print('The file movies.csv has been saved in current folder.')
-        elif file == 2:
-            data.to_excel('movies.xlsx',index=None)
+            file_name = 'movies_list' + tag_name + '.csv'
+            data.to_csv(file_name, index = None)
         else:
-            print('ERROR: Invalid file extension')
+            file_name = 'movies_list' + tag_name + '.xlsx'
+            data.to_excel(file_name, index = None)
+        print('The file "' + file_name + '" has been saved in current folder.')
     except:
-        print('ERROR: Something went wrong saving csv or xlsx file.')
+        print('ERROR: Something went wrong saving "' + file_name + '"')
 
 
 if __name__ == '__main__':
-    website = input('Please select the website:\n1. IMBD\n2. Cinemagia\nEnter the number: ')
-    rating = input('\nPlease enter the start rating for your movies:\nEnter the number: ')
-    file = input('\nPlease select extension for final document:\n1. csv\n2. xlsx\nEnter the number: ')
+    website = int(input('\nPlease select the website:\n1. IMBD\n2. Cinemagia\nEnter the number: '))
+    rating = float(input('\nPlease enter the start rating for your movies:\nEnter the number: '))
+    file = int(input('\nPlease select extension for final document:\n1. csv\n2. xlsx\nEnter the number: '))
     print('')
-    get_movies_by_rating(website, rating, file)
+    get_movies(website, rating, file)
