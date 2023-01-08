0fc60b86cdb153317c15ec151ebb309e666bdf54
cornelcristes <petrucornel.cristea@gmail.com>
2023-01-08 21:47:32 +0200

    ex4 update

--- ex4/4_vowels_and_consonants.py
+++ ex4/4_vowels_and_consonants.py
@@ -2,15 +2,15 @@
 def get_VowelsAndConsonants(word):
     w_number = 0
     c_number = 0
-    forb_list = '0123456789!"£$%^&*()_+=¬`\|<>,./?''#~}{[]'
-    w_list = 'AEIOU'
+    forb_chars = '0123456789!"£$%^&*()_+=¬`\|<>,./?''#~}{[]'
+    w_chars = 'AEIOU'
 
     for letter in word:
         upper_letter = letter.upper()
-        if upper_letter in forb_list:
+        if upper_letter in forb_chars:
             print('ERROR: Please enter a valid word')
             return
-        if upper_letter in w_list:
+        if upper_letter in w_chars:
             w_number += 1 
         else:
             c_number += 1
